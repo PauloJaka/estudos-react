045c07cf1166e86ef3a279680c5736236356563e
PauloJaka <zpaulogamer@gmail.com>
2023-07-20 11:29:29 -0300

    ambiente de desenvolvimento

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle[hash].js',
