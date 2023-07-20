9346756c9a5c14fcb503a7f2060a2d46a86856d5
Paulo Henrique <ti.pauloh@ipti.org.br>
2023-07-20 16:46:33 -0300

    react props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Post from "./Post";
 
 function App () {
     return (
     <>
-    <h1>ddd</h1>
+        <h1>dddd</h1>
+        <Post title="Titulo 01" />
+        <Post />
+        <Post />
     </>   
     );
 }
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Post(props) {
+    console.log(props)
+    return(
+        <article>
+            <strong>dd</strong><br />
+            <small>ddd</small>
+        </article>
+    );
+}
