242332196d3ac6a66295cac257fc99d4c6075e35
PauloJaka <zpaulogamer@gmail.com>
2023-07-24 23:04:38 -0300

    feacture

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Post from "./Post";
 
+const post = [
+    {title: "Title#01" , subtitle: "Sub#01", likes:20},
+    {title: "Title#01" , subtitle: "Sub#01", likes:20},
+    {title: "Title#01" , subtitle: "Sub#01", likes:20}]
+
+
 function App () {
     return (
     <>
-        <h1>dddd</h1>
-        <Post 
-        title="Titulo 02221"
-        subtitle ="titulo 02" 
-        likes = {20}/>
-        <Post likes = {20} title="Titulo 01" />
-        <Post likes = {20} />
-        
+        {post.map(
+            post => (
+                <Post 
+                likes={post.likes}
+                post ={
+                    {
+                        title: post.title,
+                        subtitle:post.subtitle,
+                    }
+                }/>
+            )
+        )}
+
+
     </>   
     );
 }
